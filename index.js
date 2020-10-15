1ab14b46bf031715dc91a7f1117f7bba2e4e1b79
Shazad Hoang <shazad.hoang@gmail.com>
2020-10-15 15:22:28 +0200

    Td-Algo #4

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let x = Math.ceil(Math.random() * 5);
+let x = Math.ceil(Math.random() * 20);
 
-if (x === 1) {
-  console.log("Hello!!!");
-} else if (x === 2) {
-  console.log("Salut!!!!");
+if (x <= 10) {
+  console.log(x);
+  console.log("Bof!!!");
+} else if (x > 10 && x <= 12) {
+  console.log(x);
+  console.log("Passable!!!");
+} else if (x > 12 && x <= 14) {
+  console.log(x);
+  console.log("Assez-bien!!!");
+} else if (x > 14 && x <= 16) {
+  console.log(x);
+  console.log("Bien!!!");
 } else {
-  console.log("Yo!!!");
+  console.log(x);
+  console.log("Très-bien!!!");
 }
